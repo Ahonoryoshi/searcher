5499040715e268ec071f09c82e55e57b9a81f8ae
Ahonoryoshi <yoshiahona@gmail.com>
2022-04-21 14:51:10 +0300

    add properties to user obect using a constructor

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,8 @@
 export class User {
+    constructor(
+        public login:string,
+        public avatar_url:string,
+        public url:string,
+        public id:number,
+        public public_repos:number){}
 }
