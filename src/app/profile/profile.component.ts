1557f0b5213aa4f59ac7e52f23ed866066006e07
Ahonoryoshi <yoshiahona@gmail.com>
2022-04-21 15:12:44 +0300

    add interface

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { User } from '../user';
 
 @Component({
   selector: 'app-profile',
@@ -8,9 +9,19 @@ import { HttpClient } from '@angular/common/http'
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user:User = new User('','','',0,0)
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+
+    interface ApiResponse{
+      login:string;
+      avatar_url:string;
+      url:string;
+      id:number;
+      public_repos:number;
+    }
   }
 
 }
