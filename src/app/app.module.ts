667d5764d5a484867cfc1d6c2f78090b67bd6535
Ahonoryoshi <yoshiahona@gmail.com>
2022-04-23 08:44:20 +0300

    add a time pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { TimePipe } from './time.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    TimePipe
   ],
   imports: [
     BrowserModule,
